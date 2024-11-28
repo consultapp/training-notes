c228ef724fad38f591a7f53c490157e6b2a562e2
Dmitry Egorov <info@consultapp.ru>
2024-11-28 16:35:09 +0300

    system base

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
       <body>
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
